091fef24064c1904995227cac16a792ee60598f1
pietrondo <pietro.capriata@gmail.com>
2015-02-04 01:13:04 +0100

    file

--- studenti/palestra.js
+++ studenti/palestra.js
@@ -1,6 +1,7 @@
 console.log("hello world");
 Studenti = new Mongo.Collection('studenti');
 if (Meteor.isClient) {
+    Meteor.subscribe("stud");
     Template.studenti.events({
             'click td': function () {
                 console.log("You clicked td" + this.name);
@@ -9,6 +10,8 @@ if (Meteor.isClient) {
                 console.log(studente);
             },
             'click .increment': function () {
+                Meteor.call('sendLogMessage');
+
                 var selezione = Session.get('SceltoStudente');
                 Studenti.update(selezione, {
                     $inc: {
@@ -31,51 +34,62 @@ if (Meteor.isClient) {
 
 
     );
-Template.aggiungistudenti.events({
-    'submit form': function (event) {
-        var currentUserId = Meteor.userId();
-        event.preventDefault();
-        var nomeStudente = event.target.studentenom.value;
-        console.log(nomeStudente);
-        console.log("baubau");
-        Studenti.insert({
-nome : nomeStudente,
-presenze: 0,
-            creato: currentUserId
-});
+    Template.aggiungistudenti.events({
+        'submit form': function (event) {
+                        event.preventDefault();
 
-    },
-    'click .remove': function(){
-var selezione = Session.get('SceltoStudente');
-        Studenti.remove(selezione);
-}
-});
-Template.studenti.helpers({
-    'studenti': function () {
-        var currentUserId = Meteor.userId();
-        return Studenti.find(
-            {creato: currentUserId},{
-            sort: {
-                presenze: -1
-            }}
-        )
-    },
-    'selectedClass': function () {
-        var studentiId = this._id;
-        var studentiSelezionati = Session.get('SceltoStudente');
-        if (studentiId == studentiSelezionati) {
-            return "selected"
+
+            var studente = event.target.studentenom.value;
+Meteor.call('inserisciStudenti', studente);
+
+
+        },
+        'click .remove': function () {
+            var selezione = Session.get('SceltoStudente');
+            Meteor.call('rimuoviStudenti',selezione);
+
         }
-    },
-    'mostraStudenti': function () {
-        var SceltoStudente = Session.get('SceltoStudente');
-        return Studenti.findOne(SceltoStudente)
-    }
+    });
+    Template.studenti.helpers({
+        'studenti': function () {
+            var currentUserId = Meteor.userId();
+            return Studenti.find()
+        },
+        'selectedClass': function () {
+            var studentiId = this._id;
+            var studentiSelezionati = Session.get('SceltoStudente');
+            if (studentiId == studentiSelezionati) {
+                return "selected"
+            }
+        },
+        'mostraStudenti': function () {
+            var SceltoStudente = Session.get('SceltoStudente');
+            return Studenti.findOne(SceltoStudente)
+        }
 
 
-});
+    });
 }
 
 if (Meteor.isServer) {
-    console.log("Hello server");
+    Meteor.publish('stud', function(){
+return Studenti.find()
+});
+    Meteor.methods({
+    
+        'inserisciStudenti': function (playerNameVar) {
+            var currentUserId = Meteor.userId();
+            console.log(currentUserId);
+            console.log(Studenti.findOne("KRdE4kGWJCn2WizTY"));
+            Studenti.insert({
+                nome: playerNameVar,
+                giorno:"febbraio",
+                presenze: 0,
+                creato: currentUserId
+            });
+        },
+        'rimuoviStudenti': function(selezione){
+Studenti.remove(selezione);
+        }
+    });
 }
